c75d3af0e6a0774571dbed2f8cbe4c92d347020c
pras7enjit <pras7enjit@gmail.com>
2017-06-20 09:29:31 +0530

    mongolab url

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
-	// "url": "mongodb://app_weather:app_weather@ds011912.mlab.com:11912/app_weather",
-	// "name": "app_weather"
-	"url": "mongodb://localhost:27017/restaurant",
-	"name":"restaurant"
+	"url": "mongodb://restaurantdatabase:restaurantdatabase@ds143131.mlab.com:43131/restaurantdatabase",
+	"name": "restaurantdatabase"
+	// "url": "mongodb://localhost:27017/restaurant",
+	// "name":"restaurant"
 }
